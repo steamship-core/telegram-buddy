dffe5cba674d9dd46dd53a458dfe5f31b36acb52
dkolas <4643013+dkolas@users.noreply.github.com>
2023-04-11 13:17:53 -0400

    Don't respond to the same message twice

--- src/api.py
+++ src/api.py
@@ -1,6 +1,6 @@
 """Description of your app."""
 import json
-from typing import Type
+from typing import Type, Optional
 
 import requests
 from steamship.invocable import Config, post, PackageService, InvocableResponse
@@ -50,24 +50,43 @@ class TelegramBuddy(PackageService):
         # TODO: must reject things not from the package
         message_text = message['text']
         chat_id = message['chat']['id']
+        message_id = message['message_id']
         logging.info(f"{message_text} {chat_id} {type(chat_id)}")
-        response = self.prepare_response(message_text, chat_id)
-        self.send_response(chat_id, response)
+        try:
+            response = self.prepare_response(message_text, chat_id, message_id)
+        except SteamshipError as e:
+            response = f"Sorry, I encountered an error while trying to think of a response: {e.message}"
+        if response is not None:
+            self.send_response(chat_id, response)
 
         return InvocableResponse(string="OK")
 
 
-    def prepare_response(self, message_text: str, chat_id: int) -> str:
+    def prepare_response(self, message_text: str, chat_id: int, message_id: int) -> Optional[str]:
         """ Use the LLM to prepare the next response by appending the user input to the file and then generating. """
         chat_file = self.get_file_for_chat(chat_id)
 
+        if self.includes_message(chat_file, message_id):
+            return None
+
         # TODO: limit total tokens passed
-        chat_file.append_block(text=message_text, tags=[Tag(kind=TagKind.ROLE, name=RoleTag.USER)])
+        chat_file.append_block(text=message_text, tags=[
+            Tag(kind=TagKind.ROLE, name=RoleTag.USER),
+            Tag(kind="message_id", name=str(message_id))
+        ])
         generate_task = self.gpt4.generate(input_file_id=chat_file.id, append_output_to_file=True, output_file_id=chat_file.id)
 
-        # TODO: handle errors here (including moderated input)
+        # TODO: handle moderated input error
         generate_task.wait()
         return generate_task.output.blocks[0].text
+
+    def includes_message(self, file: File, message_id: int):
+        """Determine if the message ID has already been processed in this file by checking Block tags."""
+        for block in file.blocks:
+            for tag in block.tags:
+                if tag.kind == "message_id" and tag.name == str(message_id):
+                    return True
+        return False
 
     def get_file_for_chat(self, chat_id: int) -> File:
         """ Find the File associated with this chat id, or create it """
